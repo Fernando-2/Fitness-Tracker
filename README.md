33b12341a60cbdc4983353776119775fc24a64e0
Fernando Rivas <fernandorivascastillo011@gmail.com>
2020-10-26 00:49:48 -0400

    cleaning up code and seeing how the read me looks

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Fitness-Tracker
+The fitness tracker allows you to input workouts and keep track of them allows you to put select data about workout
+# How It Works and technologies used
+This application is made using mongodb javascript and deployed using mongo atlas and heroku 
+## Starting the code
+I started out with making the models index.js and workout.js think of workout js the table for the information with bit a code for how long a workout is and such while index.js is just exporting the workout.
+### Then
+I made all the html routes to switch from page to page as well as the api routes to get post and put data
+
--- server.js
+++ server.js
@@ -14,75 +14,53 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
 
 app.use(express.static(path.join(__dirname, "public")));
 //API ROUTES
-app.post("/api/workouts", function (req, res) {
-    console.log("creating new workout")
-    console.log("req.body is : ", req.body)
-    const workout = req.body;
-    workout.exercises = [{
-        type: req.body.type,
-        name: req.body.name,
-        distance: req.body.distance,
-        duration: req.body.duration,
-
-
-        weight: req.body.weight,
-        sets: req.body.sets,
-        reps: req.body.reps
-    }]
-
-    db.Workout.create({})
-        .then(data => res.json(data))
+const db = require("../models");
+module.exports = function (app) {
+    app.post("/api/workouts", function (req, res) {
+        const workout = req.body;
+        workout.exercises = [{
+            type: req.body.type,
+            name: req.body.name,
+            distance: req.body.distance,
+            duration: req.body.duration,
+            weight: req.body.weight,
+            sets: req.body.sets,
+            reps: req.body.reps
+        }]
+        db.Workout.create({})
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log(err)
+            });
+    });
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        db.Workout.findByIdAndUpdate(params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                res.json(err)
+            });
+    });
+    app.get("/api/workouts", function (req, res) {
+        db.Workout.find({
+        }).then(data => res.json(data))
         .catch(err => {
-            console.log(err)
-        })
-})
+        res.json(err)
+            });
+    
+    });
+    app.get("/api/workouts/range", function (req, res) {
+        db.Workout.find({})
+            .then(data => res.json(data))
+            .catch(err => {
+                res.json(err)
+            });
+    
+    });
+}
 
-
-//put  /api/workouts/:id
-// app.put("/api/workouts/:id", jsonParser, function (req, res) {
-app.put("/api/workouts/:id", ({ body, params }, res) => {
-    //     console.log("updating workout data")
-    console.log("put req.params.id: ", params.id)
-    console.log("put req.body: ", body)
-    db.Workout.findByIdAndUpdate(params.id,
-        { $push: { exercises: body } },
-        { new: true, runValidators: true }
-    )
-        .then(data => res.json(data))
-        .catch(err => {
-            res.json(err)
-        })
-});
-
-
-
-
-//get  /api/workouts/ get all workouts
-app.get("/api/workouts", function (req, res) {
-    // console.log("the req.body is: ", req.body)
-
-    db.Workout.find({
-
-    })
-        .then(data => res.json(data))
-        .catch(err => {
-            res.json(err)
-        })
-
-})
-
-
-//get  /api/workouts/range (last 7 days: limit 7)
-app.get("/api/workouts/range", function (req, res) {
-    // console.log("the req.body is: ", req.body)
-
-    db.Workout.find({})
-        .then(data => res.json(data))
-        .catch(err => {
-            res.json(err)
-        })
-
-})
 //HTML ROUTES kept receiving error that public folder wasn't in routes put public in routes then css broke will figure out problem later
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
